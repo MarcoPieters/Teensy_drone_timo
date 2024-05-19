5d06a25f59067851eafa2dc9b06901e94c7a5ba7
MarcoPieters <marco.pieters@ziggo.nl>
2024-05-19 19:28:33 +0200

    IBUS

--- examples/Read_Ibus_reciever.cpp
+++ examples/Read_Ibus_reciever.cpp
@@ -3,16 +3,16 @@
 // Define the serial port for iBUS communication
 #define IBUS_SERIAL Serial2
 
+#define DEBUG
+
 // Buffer to store incoming iBUS data
 uint8_t ibusBuffer[30];
-int8_t StartByteWrite1 = 0x20;
-int8_t StartByteWrite2 = 0x40;
 uint8_t ibusWBuffer[28] = {0xdc, 0x05, 0xd0, 0x07, 0xe8, 0x03, 0xd0, 0x07, 0xdc, 0x05, 0xd0, 0x06, 0xd0, 0x07, 0xd0, 0x08, 0xd0, 0x09, 0xd0, 0x0a, 0xd0, 0x0b, 0xd0, 0x0c, 0xd0, 0x0d, 0xd0, 0x0e}; // packet: 2 bytes header + 2 * 14 bytes data channels + 2 bytes checksum
 uint32_t LoopTimer;
 uint32_t LoopTimer2;
 
 // Function to calculate checksum
-uint16_t calculateChecksum(uint8_t *buffer, uint8_t length, uint8_t startByte1, uint8_t startByte2) {
+uint16_t calculateChecksum(uint8_t startByte1, uint8_t startByte2,uint8_t *buffer, uint8_t length) {
   uint16_t checksum = 0xFFFF;
   checksum -= startByte1;
   checksum -= startByte2;
@@ -34,7 +34,7 @@ bool readIBUSPacket(uint8_t *buffer, int length, uint32_t timeout) {
 }
 
 void sendIBUSWriteBuffer(uint8_t startByte1, uint8_t startByte2, uint8_t *buffer, size_t length) {
-  uint16_t checksum = calculateChecksum(buffer, length, startByte1, startByte2);
+  uint16_t checksum = calculateChecksum(startByte1, startByte2,buffer, length);
   uint8_t checksumLow = checksum & 0xFF;
   uint8_t checksumHigh = (checksum >> 8) & 0xFF;
 
@@ -45,13 +45,6 @@ void sendIBUSWriteBuffer(uint8_t startByte1, uint8_t startByte2, uint8_t *buffer
   }
   Serial7.write(checksumLow);
   Serial7.write(checksumHigh);
-
-  #ifdef DEBUG
-  Serial.print("Sent Checksum: ");
-  Serial.print(checksumHigh, HEX);
-  Serial.print(" ");
-  Serial.println(checksumLow, HEX);
-  #endif
 }
 
 void setup() {
@@ -86,7 +79,7 @@ void loop() {
         uint16_t receivedChecksum = ibusBuffer[28] | (ibusBuffer[29] << 8);
 
         // Calculate the checksum of the received packet including start bytes
-        uint16_t calculatedChecksum = calculateChecksum(ibusBuffer, 28, startByte1, startByte2);
+        uint16_t calculatedChecksum = calculateChecksum(startByte1, startByte2,ibusBuffer, 28);
 
         if (micros() - LoopTimer > 200000) {
           // Print raw data for debugging
