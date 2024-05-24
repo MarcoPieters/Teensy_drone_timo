79e2b6e61df2b804e6a3059b184f49897f8d62cd
MarcoPieters <marco.pieters@ziggo.nl>
2024-05-24 15:13:40 +0200

    Sensor data

--- README.md
+++ README.md
@@ -8,16 +8,19 @@
 ##Read GYRO ACCELERATION sensor MPU6050 and use sensor fusion with complementary filtering  
 ##Read barometer sensor BMP280  
 
-#**IBUS protocol**  
+#**IBUS protocol Flysky reciever servo and sensor data**  
 https://betaflight.com/docs/wiki/guides/current/ibus-telemetry  
 https://github.com/bmellink/IBusBM  
 
 Update readmefile from visual studio code.
 ![e14cbd0e7d0f94f7baf772b03181dbc9-1459005922](https://github.com/MarcoPieters/Teensy_drone_timo/assets/168355731/8286e44f-b898-409d-b2d7-942577c88db1)
 
-Sensor fusion accelerometers and gyroscopes.
+#**Sensor fusion accelerometers and gyroscopes.**  
 https://www.digikey.nl/nl/articles/apply-sensor-fusion-to-accelerometers-and-gyroscopes
 
-IMU depth
-Hrisko, J. (2021). Gyroscope and Accelerometer Calibration with Raspberry Pi. Maker Portal.
-https://makersportal.com/blog/calibration-of-an-inertial-measurement-unit-imu-with-raspberry-pi-part-ii
+#**IMU depth**
+Hrisko, J. (2021). Gyroscope and Accelerometer Calibration with Raspberry Pi. Maker Portal.  
+https://makersportal.com/blog/calibration-of-an-inertial-measurement-unit-imu-with-raspberry-pi-part-ii  
+
+#**GPS Sensor GNSS data**  
+(https://mediatum.ub.tum.de/doc/1273200/1273200.pdf)
--- src/main.cpp
+++ src/main.cpp
@@ -1,143 +1,112 @@
-#include <Arduino.h>
+#include <TinyGPS++.h>
 
-// Define the serial port for iBUS communication
-#define IBUS_SERIAL Serial2
-
-#define DEBUG
-
-// Buffer to store incoming iBUS data
-uint8_t ibusBuffer[32];
-uint8_t ibusWBuffer[28] = {0xdc, 0x05, 0xd0, 0x07, 0xe8, 0x03, 0xd0, 0x07, 0xdc, 0x05, 0xd0, 0x06, 0xd0, 0x07, 0xd0, 0x08, 0xd0, 0x09, 0xd0, 0x0a, 0xd0, 0x0b, 0xd0, 0x0c, 0xd0, 0x0d, 0xd0, 0x0e}; // packet: 2 bytes header + 2 * 14 bytes data channels + 2 bytes checksum
-uint32_t LoopTimer;
-uint32_t LoopTimer1;
+const uint32_t GPSBaud = 9600;  // GY_GPSV3_NEO_M9N GPSBaud = 38400 : M10A-5883 GPSBaud = 9600
 uint32_t LoopTimer2;
+char data;
 
-// Function to calculate checksum
-uint16_t calculateChecksum(uint8_t protocol_lenght, uint8_t protocol_command_adr,uint8_t *buffer, uint8_t length) {
-  uint16_t checksum = 0xFFFF;
-  checksum -= protocol_lenght;
-  checksum -= protocol_command_adr;
-  for (uint8_t i = 0; i < length; i++) {
-    checksum -= buffer[i];
-  }
-  return checksum;
-}
+TinyGPSPlus gps;
 
-bool readIBUSPacket(uint8_t *buffer, int length, uint32_t timeout) {
-  uint32_t startTime = millis();
-  while (IBUS_SERIAL.available() < length) {
-    if (millis() - startTime >= timeout) {
-      Serial.print("Time out");
-      return false; // Timeout occurred
-      
+bool checkGPSConnection() {
+  // Check for data from GPS module
+  for (int i = 0; i < 10; ++i) {
+    if (Serial2.available()) {
+      return true;
     }
+    delay(500); // Wait 500ms before checking again
   }
-  IBUS_SERIAL.readBytes(buffer, length);
-  return true; // Successfully read the packet
-}
-
-void sendIBUSWriteBuffer(uint8_t protocol_lenght, uint8_t protocol_command_adr, uint8_t *buffer, size_t length) {
-  uint16_t checksum = calculateChecksum(protocol_lenght, protocol_command_adr, buffer, length);
-  uint8_t checksumLow = checksum & 0xFF;
-  uint8_t checksumHigh = (checksum >> 8) & 0xFF;
-
-  Serial7.write(protocol_lenght);
-  Serial7.write(protocol_command_adr);
-  for (size_t i = 0; i < length; i++) {
-    Serial7.write(buffer[i]);
-  }
-  Serial7.write(checksumLow);
-  Serial7.write(checksumHigh);
+  return false;
 }
 
 void setup() {
-  Serial.begin(115200); // Initialize serial monitor for debugging
-  IBUS_SERIAL.begin(115200); // Initialize iBUS serial communication
-  Serial7.begin(115200); // Initialize secondary serial communication
-  pinMode(LED_BUILTIN, OUTPUT); // Initialize LED pin
-  LoopTimer = micros();
-  LoopTimer1 = micros();
+  Serial.begin(115200);
+  Serial2.begin(GPSBaud);
+
+  // Set pin modes and initial states
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);  
+
+  // Check GPS connection
+  if (!checkGPSConnection()) {
+    Serial.println("GPS module not detected. Please check the connection.");
+    while (1); // halt the program
+  } else {
+    Serial.println("GPS module connected successfully.");
+  }
+  
   LoopTimer2 = micros();
 }
 
 void loop() {
-  static uint8_t protocol_lenght = 0;
-  static uint8_t protocol_command_adr = 0;
-  static uint8_t protocol_command = 0;
-  static uint8_t protocol_address = 0;
-
-  // Non-blocking delay for every 7 milliseconds
-  
-  if (micros() - LoopTimer1 > 7000) { 
-    sendIBUSWriteBuffer(0x20, 0x41, ibusWBuffer, 28);
-    LoopTimer1 = micros();
-    }
-
-  // Look for the start bytes
-  if (IBUS_SERIAL.available()>0){
-    if (readIBUSPacket(ibusBuffer, 2, 10)) { // 10 milisecond timeout
-      protocol_lenght = ibusBuffer[0] ;
-      protocol_command_adr = ibusBuffer[1]; // bit 4..7 command ; bit 0..3 address
-      protocol_command = protocol_command_adr & 0xF0; //0x40 servomode; 0x80 discover sensors 0x90 request sensortype 0xA0 request measurement
-      protocol_address = protocol_address & 0x0F;
-
-      if (protocol_lenght == 0x20 && protocol_command == 0x40) {
-        // Wait until we have enough data (28 bytes payload + 2 bytes checksum)
-        if (readIBUSPacket(ibusBuffer, protocol_lenght - 0x02, 10)) { // 10 milisecond timeout
-
-          uint16_t receivedChecksum = ibusBuffer[28] | (ibusBuffer[29] << 8);
-
-          // Calculate the checksum of the received packet including start bytes
-          uint16_t calculatedChecksum = calculateChecksum(protocol_lenght, protocol_command_adr,ibusBuffer, protocol_lenght - 0x04);
-
-          if (micros() - LoopTimer > 200000) {
-            // Print raw data for debugging
-            Serial.print("Raw Data: ");
-            Serial.print(protocol_lenght, HEX);
-            Serial.print(" ");
-            Serial.print(protocol_command_adr, HEX);
-            Serial.print(" ");
-            for (int i = 0; i < protocol_lenght - 0x02; i++) {
-              Serial.print(ibusBuffer[i], HEX);
-              Serial.print(" ");
-            }
-            Serial.print("Checksum: ");
-            Serial.print(receivedChecksum, HEX);
-            Serial.print(" Calculated: ");
-            Serial.println(calculatedChecksum, HEX);
-            LoopTimer = micros();
-          
-
-          if (receivedChecksum == calculatedChecksum) {
-            // Process the iBUS data if checksum is correct
-            for (int i = 0; i < 14; i++) {
-              uint16_t channelValue = ibusBuffer[2 * i + 1] << 8 | ibusBuffer[2 * i];
-              Serial.print("Channel ");
-              Serial.print(i + 1);
-              Serial.print(": ");
-              Serial.println(channelValue);
-            }
-          } else {
-            Serial.println("Checksum error");
-          }}
-        } else {
-          // Timeout occurred or not enough data
-          Serial.println("Packet read timeout or incomplete packet");
-        }
+  while (Serial2.available() > 0) {
+    data = Serial2.read();
+    if (gps.encode(data)) {
+      if (gps.location.isValid()) {
+        Serial.print("Latitude : ");
+        Serial.println(gps.location.lat(), 6);
+        Serial.print("Longitude: ");
+        Serial.println(gps.location.lng(), 6);
       } else {
-        // If the start bytes are not found, discard the bytes and continue
-        protocol_lenght = 0;
-        protocol_command_adr = 0;
-        Serial.println("No correct Header found in Packet");
+        Serial.println("Location: Not Available");
       }
+      if (gps.altitude.isValid()) {
+        Serial.print("Altitude: ");
+        Serial.print(gps.altitude.meters());
+        Serial.println(" meters");
+      } else {
+        Serial.println("Altitude: Not Available");
+      }
+      if (gps.speed.isValid()) {
+        Serial.print("Speed: ");
+        Serial.print(gps.speed.kmph());
+        Serial.println(" km/h");
+      } else {
+        Serial.println("Speed: Not Available");
+      }
+      if (gps.satellites.isValid()) {
+        Serial.print("Number of Satellites: ");
+        Serial.println(gps.satellites.value());
+      } else {
+        Serial.println("Number of Satellites: Not Available");
+      }
+      if (gps.course.isValid()) {
+        Serial.print("Course (heading): ");
+        Serial.print(gps.course.deg());
+        Serial.println(" degrees");
+      } else {
+        Serial.println("Course (heading): Not Available");
+      }
+      if (gps.date.isValid()) {
+        Serial.print("Date UTC: ");
+        Serial.print(gps.date.month());
+        Serial.print("/");
+        Serial.print(gps.date.day());
+        Serial.print("/");
+        Serial.println(gps.date.year());
+      } else {
+        Serial.println("Date: Not Available");
+      }
+      if (gps.time.isValid()) {
+        Serial.print("Time UTC: ");
+        if (gps.time.hour() < 10) Serial.print("0");
+        Serial.print(gps.time.hour());
+        Serial.print(":");
+        if (gps.time.minute() < 10) Serial.print("0");
+        Serial.print(gps.time.minute());
+        Serial.print(":");
+        if (gps.time.second() < 10) Serial.print("0");
+        Serial.println(gps.time.second());
+      } else {
+        Serial.println("Time: Not Available");
+      }
+      Serial.println();
     }
   }
 
-  if (micros() - LoopTimer2 > 200000) {
+  if (micros() - LoopTimer2 > 400000) {
     // Toggle LED state
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-
+    
     // Reset LoopTimer for the next iteration
     LoopTimer2 = micros();
-  }
+  }  
 }
